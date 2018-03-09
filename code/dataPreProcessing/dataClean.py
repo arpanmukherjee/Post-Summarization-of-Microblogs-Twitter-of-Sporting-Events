835f94bed823112a57f6ab7a9e639584f4c2c972
tiwariShubhi <shubhitiwari23@gmail.com>
2018-03-09 22:46:13 +0530

    data cleaned

--- code/dataPreProcessing/dataClean.py
+++ code/dataPreProcessing/dataClean.py
@@ -9,25 +9,66 @@
 
 import csv
 
-
+tweet = ""
 dat = []
+duplicate =0
+input = '/media/adalove/WorkDrive/M.Tech/Sem2/IR/project/project_IR/Post-Summarization-of-Microblogs-Twitter-of-Sporting-Events/Dataset/Dataset collected/TW_RMPSGUCL_15feb_SecRemoved.csv'
+output = '/media/adalove/WorkDrive/M.Tech/Sem2/IR/project/project_IR/Post-Summarization-of-Microblogs-Twitter-of-Sporting-Events/Dataset/Dataset collected/TW_RMPSGUCL_15feb_cleanData.csv'
+
+
 #read file
-with open('/media/adalove/WorkDrive/M.Tech/Sem2/IR/project/project_IR/Post-Summarization-of-Microblogs-Twitter-of-Sporting-Events/Dataset/Dataset collected/TW_RMPSGUCL_15feb.csv','r') as f:
+with open(input,'r') as f:
     reader = csv.reader(f)
 
     for row in reader:
-        #remove seconds
+
         list = []
         if len(row) != 2:
             continue
-        t =str(row[0])[0:16]
-        #print t
+
+        # remove seconds
+        #t =str(row[0])[0:16]
+        t= row[0]
+
+        tweet = row[1]
+        # remove url
+        urlIndex = row[1].find("https")
+        if urlIndex!=-1:
+            nextSpace = row[1].find(" ",urlIndex)
+            if nextSpace ==-1:
+                #end of string
+                tweet = row[1][:urlIndex]
+            else:
+                tweet = row[1][:urlIndex] + row[1][nextSpace+1:len(row[1])]
+        #print tweet
+
+        #remove retweets
+        if tweet.find("RT @")==0:
+           continue
+
+        #remove duplicate tweets
+        #accumulated due to twitter API network error
+        for pair in dat:
+            #print "here"
+            if t == pair[0] and tweet == pair[1]:
+                print t
+                print tweet
+                duplicate =1
+                break
+
+        if duplicate==1:
+            duplicate=0
+            continue
+
+
+
         list.append(t)
-        list.append(row[1])
+        list.append(tweet)
         dat.append(list)
 
+
 #writing to csv
-with open('/media/adalove/WorkDrive/M.Tech/Sem2/IR/project/project_IR/Post-Summarization-of-Microblogs-Twitter-of-Sporting-Events/Dataset/Dataset collected/TW_RMPSGUCL_15feb_SecRemoved.csv','w+') as f1:
+with open(output,'w+') as f1:
     writer = csv.writer(f1)
     writer.writerows(dat)
 
