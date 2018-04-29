215db19d9762ac48f6783b986974585c67bc7677
Arpan Mukherjee <player.arpan@gmail.com>
2018-04-29 20:43:13 +0530

    Update csv_plotter.py

--- code/csv_plotter.py
+++ code/csv_plotter.py
@@ -2,7 +2,8 @@ import csv
 import matplotlib.pyplot as plt;plt.rcdefaults()
 import numpy as np
 
-csv_path = "/media/adalove/WorkDrive/M.Tech/Sem2/IR/project/project_IR/Post-Summarization-of-Microblogs-Twitter-of-Sporting-Events/Dataset/cleanedData/TW_INDSA_13feb_Cleaned2.csv"
+csv_path = "/Users/arpn/Google Drive/Semester2/Information Retrieval/Post-Summarization-of-Microblogs-Twitter-of-Sporting-Events/Dataset/cleanedData/TW_INDSA_13feb_Cleaned2.csv"
+
 data_dict = {}
 
 
@@ -17,8 +18,8 @@ def csv_reader(file_obj):
     for row in reader:
         key = row[0][:-3]
         if key not in data_dict.keys():
-            data_dict[key] = set()
-        data_dict[key].add(row[1])
+            data_dict[key] = []
+        data_dict[key].append(row[1])
 
 
 def build_data_graph():
@@ -30,13 +31,16 @@ def build_data_graph():
     performance = []
     for i in temp:
         performance.append(len(data_dict[i]))
-
+    mean = np.mean(performance)
+    std_dev = np.std(performance)
+    threshold = mean+2*std_dev
     plt.bar(y_pos, performance, align='center', alpha=0.5)
     plt.xticks(y_pos, objects)
     plt.ylabel('No of tweetes')
     plt.title('Comparison of no of tweets with time')
 
     plt.show()
+    return performance, threshold
 
 
 
@@ -46,6 +50,21 @@ with open(csv_path, "rb") as file_obj:
     except:
         print("Ignore")
 
+performance, threshold = build_data_graph()
+imp_moments = 0
+for i in range(len(performance)):
+    if performance[i] >= threshold:
+        imp_moments += 1
+print("Important Moments: " + str(imp_moments))
+# print("Maximum tweets in a minute: "+str(max([len(data_dict[i]) for i in data_dict.keys()])))
+
+threshold_tweets = {}
+for key in sorted(data_dict.keys()):
+    if len(data_dict[key]) > threshold:
+        threshold_tweets[key] = data_dict[key]
+with open('threshold_data.csv', 'wb') as csv_file:
+    writer = csv.writer(csv_file)
+    for key in threshold_tweets.keys():
+        for j in range(len(threshold_tweets[key])):
+            writer.writerow([key, threshold_tweets[key][j]])
 build_data_graph()
-
-
